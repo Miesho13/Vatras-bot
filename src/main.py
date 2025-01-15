df7c4cee38abfeee6cb6ab6c2a9c2c1f192b9f75
Miesho <marcin.ryzewskii@gmail.com>
2025-01-15 22:01:56 +0100

    Add commadn for join to chanel

--- src/main.py
+++ src/main.py
@@ -1,29 +1,34 @@
 from dotenv import load_dotenv
 import os
 import discord
+from discord.ext import 
 
 class VatrasBot(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
-
         if message.author == self.user:
             return
 
-        await message.channel.send(f'You said: {message.content}')
+        if self.user.mentioned_in(message) and "join" in message.content.lower():
+            if message.author.voice and message.author.voice.channel:
+                voice_channel = message.author.voice.channel
+                await voice_channel.connect()
+            else:
+                await message.channel.send("You are not in a voice channel!")
 
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = VatrasBot(intents=intents)
 
 def main():
     load_dotenv()
     env = os.getenv('BOT_KAY')
+    bot.run(f"{env}")
 
-    intents = discord.Intents.default()
-    intents.message_content = True
-
-    client = VatrasBot(intents=intents)
-    client.run(f"{env}")
-    
 if __name__ == "__main__":
     main()
